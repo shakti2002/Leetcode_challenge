96c6e55aa10a1d8e44d21e1477fd8b5bb1aec30c
shakti2002 <109802187+shakti2002@users.noreply.github.com>
2023-03-05 12:11:35 -0800

    Create 06-03-2023.cpp

---
+++ 21th_206._Reverse_Linked_Lis/06-03-2023.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev =NULL;
+        ListNode* curr=head;
+        ListNode* forward=NULL;
+        while(curr!=NULL){
+            forward = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = forward;
+        }
+        return prev;
+    
+    }
+};
